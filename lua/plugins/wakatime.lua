ec251d6ba8bbbb82b7941a3d494f27ab1a5a50fb
Alexis Jesus Buelvas Madera <alexjesus-4856@hotmail.com>
2025-02-02 23:53:36 -0500

    Add Wakatime plugin

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -3,10 +3,7 @@ return {
     "wakatime/vim-wakatime",
     lazy = false,
     -- config = function()
-    --         -- Especifica la ruta al CLI de Wakatime (instalado con pipx)
     --   vim.g.wakatime_CLIPath = vim.fn.expand("~/.local/bin/wakatime")
-    --   -- Opcional: Configuración adicional (si es necesaria)
-    --   vim.g.wakatime_ScreenRedraw = 0 -- Desactiva redibujado de pantalla
     -- end,
   },
 }
